1fafb90d69d8fc77867d9bcabf36cf53aeb65969
Alelseizor <LexaCR7@bk.ru>
2022-05-30 14:13:28 +0300

    State(DayState,WeekState,DepartmentState)

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 type State interface {
 	clickButton()
 	clickUndo()
-	changeSchedule(schedule Schedule)
+	changeSchedule(schedule *Schedule)
 }
 
 type Schedule struct {
@@ -14,6 +14,47 @@ func (schedule *Schedule) changeState(state State) {
 
 }
 
+type DayState struct {
+	schedule Schedule
+}
+
+func (day DayState) clickButton() {
+
+}
+func (day DayState) clickUndo() {
+
+}
+func (day DayState) changeSchedule(schedule *Schedule) {
+
+}
+
+type WeekState struct {
+	schedule Schedule
+}
+
+func (week WeekState) clickButton() {
+
+}
+func (week WeekState) clickUndo() {
+
+}
+func (week WeekState) changeSchedule(schedule *Schedule) {
+
+}
+
+type DepartmentState struct {
+	schedule Schedule
+}
+
+func (department DepartmentState) clickButton() {
+
+}
+func (department DepartmentState) clickUndo() {
+
+}
+func (department DepartmentState) changeSchedule(schedule *Schedule) {
+
+}
 func main() {
 
 }
